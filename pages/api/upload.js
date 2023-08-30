e6338bfa0cf79c0c4189d1fa846b66ef5b67236e
Rudis <82242977+krakatua@users.noreply.github.com>
2023-08-29 21:57:41 -0400

    Features
    Some UI and responsive changes, some changes on the format of the code, new Order Page Its been develop

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -7,7 +7,6 @@ import { mongooseConnect } from "@/lib/mongoose";
 
 export default async function handle(req, res) {
   await mongooseConnect();
-    await isAdminRequest(req,res)
   const form = new multiparty.Form();
   const {fields, files} = await new Promise((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
