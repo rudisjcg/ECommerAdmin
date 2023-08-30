4874c65fc128be0a8d3456e3a3bfa935b0f8a3ff
Rudis <82242977+krakatua@users.noreply.github.com>
2023-08-30 17:10:27 -0400

    Order page
    Order Page Created and configured, some styling and features are missing

--- pages/orders.js
+++ pages/orders.js
@@ -1,47 +1,51 @@
-import Layout from '@/components/Layout'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import Layout from "@/components/Layout";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function OrdersPage() {
   const [orders, setOrders] = useState([]);
   useEffect(() => {
-    axios.get('/api/orders').then(res => {
+    axios.get("/api/orders").then((res) => {
       setOrders(res.data);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <Layout>
       <h1>Orders</h1>
-      <table className='basic m-4'>
-    <thead>
-        <tr>
+      <table className="basic m-4">
+        <thead>
+          <tr>
             <th>Date</th>
+            <th>Paid</th>
             <th>Recipient</th>
             <th>Products</th>
-        </tr>
-    </thead>
-    <tbody>
-{orders.length > 0 && orders.map((order) => (
-  <tr key={order._id}>
-    <td>{order.createdAt}</td>
-    <td>
-      {order.name} {order.email} <br/>
-      {order.city} {order.postalCode} {order.country} <br/>
-      {order.streetAddress}
-    </td>
-    <td>
-      {order.line_items.map((l) => (
-        <>
-        {l?.product_data?.name} x {l?.quantity} <br/>
-          {JSON.stringify(l)}<br/>
-        </>
-      ))}
-    </td>
-  </tr>
-))}
-    </tbody>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order) => (
+              <tr key={order._id}>
+                <td>{new Date(order.createdAt).toLocaleString()}</td>
+                <td className={order.paid ? "text-green-500 " : "text-red-600"}>
+                  {order.paid ? "YES" : "NO"}
+                </td>
+                <td>
+                  {order.name} {order.email} <br />
+                  {order.city} {order.postalCode} {order.country} <br />
+                  {order.streetAddress}
+                </td>
+                <td>
+                  {order.line_items.map((l) => (
+                    <>
+                      {l?.price_data.product_data?.name} x {l?.quantity} <br />
+                    </>
+                  ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
       </table>
     </Layout>
-  )
+  );
 }
